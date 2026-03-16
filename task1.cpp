9348d65468b42def7822232fbbeab79fa5c6e2f2
Mujahid <md0n211906@gmail.com>
2026-03-16 10:11:38 +0530

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -2,18 +2,24 @@
 #include<random>
 using namespace std;
 void faah(int a){
-    int x;
-    cout<<"guess the number between 0 and 1000\n";
+    int x,at=1;
+    cout<<"guess the number between 0 and 100\n";
     cin>>x;
-    while(x!=a){if(x>a){ cout<<"high\n";
+    while(x!=a)
+    {
+        at++;
+        if(x>a){ 
+            cout<<"high";
         
-    if(x-a<=5)cout<<"close\n";
+    if(x-a<=5)cout<<" and close\n";
+    else cout<<"\n";
 }
-else if(x<a){cout<<"low\n";
-    if(a-x<=5)cout<<"close\n";
+else if(x<a){cout<<"low";
+    if(a-x<=5)cout<<" and close\n";
+    else cout << "\n";
 }
 cin>>x;}
-cout<<"correct"<<endl;
+cout<<"correct and took "<<at<<" attempts\n";
 }
 int main(){
 random_device rd;
